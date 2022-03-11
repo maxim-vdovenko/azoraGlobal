b8994ca6c39b6fb13b07118ee496d4ccb2a5efd2
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2022-03-11 17:26:16 +0200

    Add

---
+++ README.md
@@ -0,0 +1,11 @@
+Link path: 
+----------------------------------------------------
+https://smarthead.net/work/azoraGlobal/pages.html
+
+https://smarthead.net/work/azoraGlobal/index.html
+
+Project work: 
+----------------------------------------------------
+init: npm i
+
+start: gulp
---
+++ src/js/index.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+  os.init()
+})
+
+const screen = {
+  xl: 1180,
+  lg: 890,
+  md: 768,
+  sm: 640,
+  xs: 500
+}
+
+const os = {
+  class: 'ios'
+}
+
+os.init = () => {
+  if (os.detect() === 'MacOS' || os.detect() === 'iOS') {
+    $('body').addClass(os.class)
+  }
+}
+
+os.detect = () => { 
+  const platform = navigator.platform.toLowerCase(),
+      iosPlatforms = ['iphone', 'ipad', 'ipod', 'ipod touch']
+
+  if (platform.includes('mac')) return 'MacOS'
+  if (iosPlatforms.includes(platform)) return 'iOS'
+  if (platform.includes('win')) return 'Windows'
+  if (/android/.test(navigator.userAgent.toLowerCase())) return 'Android'
+  if (/linux/.test(platform)) return 'Linux'
+
+  return 'unknown'
+}
+
+// const card = {
+//   init: () => {
+//     new Inputmask('9999 9999 9999 9999', {
+//         placeholder: ' ',
+//         showMaskOnHover: false,
+//         showMaskOnFocus: false
+//     }).mask('.input-card')
+  
+//     new Inputmask('99 / 99', {
+//         placeholder: ' ',
+//         showMaskOnHover: false,
+//         showMaskOnFocus: false,
+//         alias: "datetime",
+//         inputFormat: "mm/yy",
+//         greedy: false
+//     }).mask('.input-date')
+  
+//     new Inputmask('999', {
+//         placeholder: ' ',
+//         showMaskOnHover: false,
+//         showMaskOnFocus: false
+//     }).mask('.input-cvv');
+//   }
+// }
