a1b7aa054b249a2ee78ab06fddf9ffbe6b3f0a33
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2022-04-09 23:15:29 +0300

    Add step 3

--- src/js/parcel.js
+++ src/js/parcel.js
@@ -1,12 +1,30 @@
 const parcel = {
-  box: '.parcel__side-box',
-  sendMenu: '.parcel__sendMenu',
-  points: '.parcel__points',
-  courier: '.parcel__courier'
+  howSend: {
+    box: '.parcel__side-box',
+    menu: '.parcel__sendMenu',
+    points: '.parcel__points',
+    courier: '.parcel__courier'
+  },
+  table: {
+    inputBox: '.parcel__table-input',
+    inputNumbers: '.parcel__table-input-value.input-numbers',
+    buttonTop: '.parcel__table-input-top',
+    buttonBottom: '.parcel__table-input-bottom'
+  }
 }
 
 parcel.init = () => {
-  $('body').on('click', parcel.sendMenu + ' li', e => {
+  parcel.howSendFn()
+  parcel.tableFn()
+}
+
+parcel.howSendFn = () => {
+  const menuButton = parcel.howSend.menu + ' li'
+  const box = parcel.howSend.box
+  const points = parcel.howSend.points
+  const courier = parcel.howSend.courier
+ 
+  $('body').on('click', menuButton, e => {
     const th = $(e.currentTarget)
     const ind = th.index()
 
@@ -14,11 +32,35 @@ parcel.init = () => {
     th.addClass('active')
 
     if (ind) {
-      th.parents(parcel.box).find(parcel.points).removeClass('active')
-      th.parents(parcel.box).find(parcel.courier).addClass('active')
+      th.parents(box).find(points).removeClass('active')
+      th.parents(box).find(courier).addClass('active')
     } else {
-      th.parents(parcel.box).find(parcel.points).addClass('active')
-      th.parents(parcel.box).find(parcel.courier).removeClass('active')
+      th.parents(box).find(points).addClass('active')
+      th.parents(box).find(courier).removeClass('active')
+    }
+  })
+}
+
+parcel.tableFn = () => {
+  const box = parcel.table.inputBox
+  const numbers = parcel.table.inputNumbers
+  const top = parcel.table.buttonTop
+  const bottom = parcel.table.buttonBottom
+
+  $('body').on('click', top, e => {
+    const th = $(e.currentTarget)
+    const val = Number(th.parents(box).find(numbers).val())
+    th.parents(box).find(numbers).val(val + 1)
+  })
+
+  $('body').on('click', bottom, e => {
+    const th = $(e.currentTarget)
+    const val = Number(th.parents(box).find(numbers).val())
+
+    if (val > 1) {
+      th.parents(box).find(numbers).val(val - 1)
+    } else {
+      th.parents(box).find(numbers).val('')
     }
   })
 }
