2d3680e08573d8f55cab2bd36b86094f658d0b20
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2022-06-13 21:46:28 +0300

    Add all modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,7 @@ const modal = {
   bl: '.modal',
   block: '.modal__block',
   cont: '.modal__cont',
-  close: '.modal__close',
+  close: '.modal-close',
   button: '.modal-button',
   time: {
     open: 250,
@@ -18,34 +18,56 @@ modal.init = () => {
   }
   
   $('body').on('click', modal.button, e => {
-    modal.openFn($(e.currentTarget))
+    modal.openFn(e)
   })
 
-  $('body').on('click', modal.close, () => {
-    modal.closeFn()
+  $('body').on('click', modal.close, e => {
+    modal.closeFn(e)
   })
 
   $('body').on('click', e => {
     if ($(e.target).closest(modal.bl).length && !$(e.target).closest(modal.cont).length) {
-      modal.closeFn()
+      modal.closeFn(e)
     }
+  })
+
+  $('body').on('click', '.modal__address-close', e => {
+    const block = $(e.currentTarget).parents('.modal__address')
+    block.fadeOut(250, () => {
+      block.remove()
+    })
   })
 }
 
 modal.openFn = e => {
-  const id = e.attr('data-id')
+  const id = $(e.currentTarget).attr('data-id')
+
+  $(modal.bl + '.open').removeClass('open').fadeOut(modal.time.close)
   $('#' + id).addClass('open').fadeIn(modal.time.open)
 
-  $('<div class="modalFon"></div>').appendTo('body')
-
-  setTimeout(() => {
-    $('.modalFon').addClass('active')
-  }, 1)
+  if (!$('.modalFon').length) {
+    $('<div class="modalFon"></div>').appendTo('body')
+    setTimeout(() => {
+      $('.modalFon').addClass('active')
+    }, 1)
+  }
 }
 
-modal.closeFn = () => {
+modal.closeFn = e => {
+  const th = $(e.currentTarget)
   $('.modalFon').removeClass('active')
-  $(modal.bl + '.open').removeClass('open').fadeOut(modal.time.close, () => {
-    $('.modalFon').remove()
-  })
+
+  if (th.parents(modal.bl).length) {
+    
+    th.parents(modal.bl).removeClass('open').fadeOut(modal.time.close, () => {
+      if (!$(modal.bl + '.open').length) {
+        $('.modalFon').remove()
+      }
+    })
+  } else {
+    
+    $(modal.bl + '.open').removeClass('open').fadeOut(modal.time.close, () => {
+      $('.modalFon').remove()
+    })
+  }
 }
